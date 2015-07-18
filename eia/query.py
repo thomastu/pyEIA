33e4273cc17a68dbb9a65f6e6f9440eafa08adaa
Thomas Tu <ttu@lbl.gov>
2015-07-18 13:09:20 -0700

    added docstrings

--- eia/query.py
+++ eia/query.py
@@ -7,36 +7,60 @@ from string import ascii_uppercase as abc
 
 api_key = ''
 
-class BaseQuery:
+class baseQuery(object):
     """
-    Query containing methods for making requests to the EIA API.
+    Container class that caches json response for a history on any
+    get or post request. 
     """
-
-    host = "http://api.eia.gov"
-    base_skeleton = '{host}/{queryType}/?api_key={api_key}'
-    api_key = api_key
-
     def __init__(self):
         """
+        Sets initial parameters used for making requests to api.
+        self.urlSkeleton should be formatted by child classes 
         """
+        self.host = "http://api.eia.gov"
+        self.urlSkeleton = '{host}/{queryType}/?api_key={api_key}'
+        self.api_key = api_key
         self.request = None
         self.url = None
         self.history = {}
 
     def make_get_request(self, url):
+        """
+        Checks to see if desired url endpoint has been seen by self.history.
+        If not, a new request is made, its json response is added to 
+        self.history.  Returns the instance json response.  
+        """
         if url not in self.history:
             r = requests.get(url)
             self.history.update({url : r.json()})
-        return self.history.get(url)
+        return self.history[url]
 
     def make_post_request(self, url, data):
+        """
+        Simple wrapper around requests.post that returns json.  
+        Results are not stored because there is no well defined identifier for a 
+        given post request.  Useful for minimizing memory footprint.
+        Every get request has an equivalent post request.
+        """
         r = requests.post(url, data = data)
         return r.json()
 
-class CategoryQuery(BaseQuery):
+class categoryQuery(baseQuery):
 
-    base_url = base_skeleton.format(host = host, queryType = 'category', 
-        api_key = api_key)
+    def __init__(self):
+        """
+        Makes requests to the category api endpoint.  
+        Queries are defined by specifying a category id which returns
+        a list of all childcategories and childseries identifiers.
+
+        Pseudo Example : 
+        >>> x = categoryQuery()
+        >>> x.get(471)
+        {"childcategories" : ["Annual Energy Outlook", "State Energy System" ...], }
+        """
+        super(categoryQuery, self).__init__()
+        self.base_url = self.urlSkeleton.format(host = self.host, queryType = 'category', 
+            api_key = api_key)
 
     def make_url(self, category_id):
         url_skeleton = '{base}&category_id={id}'
@@ -50,14 +74,28 @@ class CategoryQuery(BaseQuery):
         return r
 
     def post(self, data):
+        """
+        ---------- Post parameters ----------
+        category_id : int or str, integer or integer-as-string that corresponds
+            to an EIA category.
+        """
         return self.make_post_request(self.base_url, data)
 
-class SeriesQuery(BaseQuery):
+class seriesQuery(baseQuery):
 
-    base_url = base_skeleton.format(host = host, queryType = 'series', 
-        api_key = api_key)
+    def __init__(self):
+        """
+        Creates a query to the series api endpoint.  Returns time series data 
+        from requested series ids.  Accepts up to 100 series ids per request. 
+        """
+        super(categoryQuery, self).__init__()
+        base_url = urlSkeleton.format(host = self.host, queryType = 'series', 
+            api_key = api_key)
 
     def make_url(self, series_id):
+        """
+        Builds the URL endpoint for a series query. 
+        """
         url_skelton = '{base}&series_id={id}'
         url = url_skeleton.format(base = self.base_url, id = series_id)
         self.url = url 
@@ -69,12 +107,21 @@ class SeriesQuery(BaseQuery):
         return r 
 
     def post(self, data):
+        """
+        ---------- Post Parameters ----------
+        :series_id: str, semi-colon delimited list-as-string of series ids.
+        Example : 
+        data = {'series_id' : 'series1_id;series_2_id;series_3_id;'}
+        self.post(data)
+        """
         return self.make_post_request(self.base_url, data)
 
-class SearchQuery(BaseQuery):
+class searchQuery(baseQuery):
 
-    base_url = base_skeleton.format(host=host, queryType='search',
-        api_key=api_key)
+    def __init__(self):
+        super(categoryQuery, self).__init__()
+        self.base_url = urlSkeleton.format(host=self.host, queryType='search',
+            api_key=self.api_key)
 
     def make_url(self, search_term, search_value, page_num=None, rows_per_page=None):
         url_skeleton= '{base}&search_term={term}&search_value="{value}"'
