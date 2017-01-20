9e9466a7ddfa6d84af65bea2afa60414f8b7a131
Thomas Tu <thomastu@users.noreply.github.com>
2017-01-20 14:17:02 -0800

    Update setup version to 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='pyeia',
-    version='0.0.1',
+    version='0.0.2',
 
     description='Python client for Energy Information Administration (EIA) API',
     long_description='',
